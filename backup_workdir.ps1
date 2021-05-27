04d12d7e662e6740378fd1da3af6b1f9c2788763
hiramiya523 <hiramiya523@gmail.com>
2021-05-27 23:25:48 +0900

    保有期間を過ぎたらバックアップを削除

--- backup_workdir.ps1
+++ backup_workdir.ps1
@@ -22,4 +22,24 @@ if((Test-Path $from_dir) -and (Test-Path $to_dir)){
     Write-Host "パス不正です"
 }
 
+Delete_overdue_back
+
 Write-Host "処理が終了いたしました"
+
+#<#
+カレントフォルダのzipファイルで、保有期間($hold_date)を過ぎたファイルを削除
+#>
+function Delete_overdue_back {
+    $today = Get-Date -DisplayHint Date
+    $zip_name_arr = Get-ChildItem  $to_dir -Include *zip -Name
+
+    # zipファイルの日付で、保有期間を過ぎていれば削除
+    foreach($zipname_i in $zip_name_arr){
+        $zip_day = [datetime]$zipname_i.Substring(0, 10)
+        $day_diff = ($today - $zip_day).Days
+
+        if($day_diff -gt $hold_date){
+            Remove-Item (Join-Path $to_dir $zipname_i)
+        }
+    }
+}
